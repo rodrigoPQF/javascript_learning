1e21eb8c1d29a66f64921e37c4b7c7173d9241bd
rodrigoPQF <rodrigopr404@gmail.com>
2021-07-05 11:38:24 -0300

    Final

--- funcao/arrowFunction1.js
+++ funcao/arrowFunction1.js
@@ -18,3 +18,32 @@ ola = () => 'Hello'
 ola = _ => 'Ola' // Possui um param
 
 console.log(ola())
+
+function isEu(vl1, vl2) {
+    return vl1 === vl2
+}
+
+console.log(false == undefined)
+
+
+const array = [3,5,5,7,9,9,1,9,2,5,7,9,2,42,12,56,87,34,23,34]
+function removeDuplicates(array) {
+
+    let newArray = []
+
+    for (let i = 0; i < array.length; i++) {
+
+            if (array.indexOf(array[i]) === i) {
+
+                    newArray.push(array[i])
+
+            }
+
+    }
+
+    return newArray
+
+}
+
+let num = ''
+console.log(num)
